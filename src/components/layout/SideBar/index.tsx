85cd20939f1be5a8867ddbf0e6c22c6edf7e5028
m-ghiasi <mahdiye71gh@gmail.com>
2025-07-02 13:06:52 +0430

    router

---
+++ src/components/layout/SideBar/index.tsx
@@ -0,0 +1,31 @@
+import type { ReactNode } from "react";
+import { NavLink } from "react-router-dom";
+
+type SideBarItemsType = {
+  label: string;
+  icon?: ReactNode;
+  path?: string;
+  onClick?: () => void;
+};
+
+type SideBarprops = {
+  menuItems: SideBarItemsType[];
+};
+export default function SideBar(props: SideBarprops) {
+  const { menuItems } = props;
+  return (
+    <div>
+      {menuItems.map((item, index) => (
+        <NavLink
+          key={index}
+          className={` flex gap-3 items-center pl-6  pb-4`}
+          to={item.path}
+        >
+          <span className="text-gray-700 ">{item.icon}</span>
+
+          <span className={` font-medium text-gray-700`}> {item.label}</span>
+        </NavLink>
+      ))}
+    </div>
+  );
+}
