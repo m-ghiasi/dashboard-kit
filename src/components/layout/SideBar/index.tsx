f36126533eccd31eca5cfcb1f10a711808405401
m-ghiasi <mahdiye71gh@gmail.com>
2025-07-07 10:46:19 +0430

    SideBar-completed

--- src/components/layout/SideBar/index.tsx
+++ src/components/layout/SideBar/index.tsx
@@ -1,8 +1,8 @@
-import type { ReactNode } from "react";
+import { useState, type ReactNode } from "react";
 import { NavLink } from "react-router-dom";
 
 type SideBarItemsType = {
-  label: string;
+  label?: string;
   icon?: ReactNode;
   path?: string;
   onClick?: () => void;
@@ -13,17 +13,19 @@ type SideBarprops = {
 };
 export default function SideBar(props: SideBarprops) {
   const { menuItems } = props;
+  const [isActive, setIsActive]= useState<string | undefined>("")
   return (
-    <div>
+    <div className={`w-22  `}>
       {menuItems.map((item, index) => (
         <NavLink
           key={index}
-          className={` flex gap-3 items-center pl-6  pb-4`}
+          className={` flex gap-3 items-center pl-6  pb-4 ${isActive === item.label? "border-l-4 border-[#454B60]":""  }`}
           to={`${item.path}`}
+          onClick={()=>setIsActive(item.label)}
         >
-          <span className="text-gray-700 ">{item.icon}</span>
+          <span className="text-[#454B60] ">{item.icon}</span>
 
-          <span className={` font-medium text-gray-700`}> {item.label}</span>
+         { item.label && <span className={` font-medium text-[#454B60]`}> {item.label}</span>}
         </NavLink>
       ))}
     </div>
